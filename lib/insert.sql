ffc1010d6a84f5eadaa7a9017cdfc1f1b9a260ae
Sam Gold <samg915757@gmail.com>
2020-12-23 15:29:41 -0700

    commit tree f3ee9f2c81ab1b5322c8d64a46e6667ae84909d9

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "M", "Brown", "Chill", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, "M", "Black", "Rowdy", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "F", "Spotted", "Unpredictable", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 14, "M", "Roan", "Uptight", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 7, "M", "Brown", "Chill", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 7, "M", "Brown", "Chill", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 7, "M", "Brown", "Chill", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 7, "M", "Brown", "Chill", True);
+
--- spec/select_spec.rb
+++ spec/select_spec.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 describe 'querying the bears table' do
   before do
     @db = SQLite3::Database.new(':memory:')
